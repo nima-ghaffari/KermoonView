bd69f09499d787a2d79a127f5f94ffdd43c7a0db
nima-ghaffari <nimaghafariir@gmail.com>
2026-01-01 23:00:01 +0330

    views started .

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from itertools import chain
+from operator import attrgetter
+from django.contrib import messages
 
-# Create your views here.
+from .models import (
+    Tour, Reservation, TourLeaderProfile, 
+    Post, PostComment, Comment, LeaderReview, User
+)
+
