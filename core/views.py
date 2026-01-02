c5a7621136bee4fb0a80790603c4437039d218a1
nima-ghaffari <nimaghafariir@gmail.com>
2026-01-02 08:33:21 +0330

    View --> add Public sec and assign view.

--- core/views.py
+++ core/views.py
@@ -8,6 +8,19 @@ from django.contrib import messages
 
 from .models import (
     Tour, Reservation, TourLeaderProfile, 
-    Post, PostComment, Comment, LeaderReview, User
-)
+    Post, PostComment, Comment, LeaderReview, User)
 
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Public sectors --> home tour weblog
+def index(request):
+    latest_tours = Tour.objects.filter(is_active=True).order_by('-id')[:3]
+    latest_posts = Post.objects.filter(is_published=True).order_by('-created_at')[:3]
+    tour_comments = Comment.objects.filter(is_approved=True).select_related('tour', 'user')
+    leader_reviews = LeaderReview.objects.filter(is_approved=True).select_related('leader', 'user')
+    latest_reviews = sorted(chain(tour_comments, leader_reviews), key=attrgetter('created_at'), reverse=True)[:4]
+    return render(request, 'index.html', {'tours': latest_tours, 'posts': latest_posts, 'reviews': latest_reviews})
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#
